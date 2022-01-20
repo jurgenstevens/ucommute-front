2240e8ede9c057708ac9d00bb7bca09c0e370d2a
jurgenstevens <jurgen.stevens@yahoo.com>
2022-01-20 17:02:53 -0600

    Use the useEffect method to fetch train data.

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Destination() {
     // make a fetch call to access list of trains by color onClick and set them to the trainList
-    const [trainList, setTrainList] = useState([])
+    const [trainData, setTrainData] = useState(null)
     const [selectedLine, setSelectedLine] = useState('')
     const [selectedStation, setSelectedStation] = useState('')
 
     const lines = ['Orange', 'Brown', 'Red', 'Blue', 'Green', 'Purple', 'Yellow']
 
     // use the fetch methodto return all of the station from the database
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACKEND_SERVER_URL}/stations`)
+            .then((res) => res.json())
+            .then((trainData) => {
+                setTrainData(trainData)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     // create a function to set the selected train line to the selectedLine hook
 
--- src/components/Origin.jsx
+++ src/components/Origin.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Origin() {
     // make a fetch call to access list of trains by color onClick and set them to the trainList
-    const [trainList, setTrainList] = useState([])
-    const [selectedLine, setSelectedLine] = useState('')
-    const [selectedStation, setSelectedStation] = useState('')
+    const [trainData, setTrainData] = useState([])
+    const [selectedLine, setSelectedLine] = useState(null)
+    const [selectedStation, setSelectedStation] = useState(null)
 
     const lines = ['Orange', 'Brown', 'Red', 'Blue', 'Green', 'Purple', 'Yellow']
 
-    // use the fetch methodto return all of the station from the database
+    // use the fetch method to return all of the station from the database
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACKEND_SERVER_URL}/stations`)
+            .then((res) => res.json())
+            .then((trainData) => {
+                setTrainData(trainData)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     // create a function to set the selected train line to the selectedLine hook
 
@@ -23,7 +33,6 @@ function Origin() {
                         <option value={line} key={line}>{line}</option>
                     )
                 })}
-
             </select>
         </div>
     )
