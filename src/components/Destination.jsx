419f0faf48554ffabfe6b28c046796d3cd289b12
jurgenstevens <jurgen.stevens@yahoo.com>
2022-01-20 15:47:28 -0600

    Set up hooks for selected lines and stations and pseudocode your functions.

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -3,8 +3,16 @@ import React, { useState } from 'react'
 function Destination() {
     // make a fetch call to access list of trains by color onClick and set them to the trainList
     const [trainList, setTrainList] = useState([])
+    const [selectedLine, setSelectedLine] = useState('')
+    const [selectedStation, setSelectedStation] = useState('')
 
     const lines = ['Orange', 'Brown', 'Red', 'Blue', 'Green', 'Purple', 'Yellow']
+
+    // use the fetch methodto return all of the station from the database
+
+    // create a function to set the selected train line to the selectedLine hook
+
+    //create a function to render the dropdown for the stations if and when the line is selected
 
     return (
         <div>
@@ -15,8 +23,8 @@ function Destination() {
                         <option value={line} key={line}>{line}</option>
                     )
                 })}
-
             </select>
+            
         </div>
     )
 }
--- src/components/Origin.jsx
+++ src/components/Origin.jsx
@@ -3,8 +3,16 @@ import React, { useState } from 'react'
 function Origin() {
     // make a fetch call to access list of trains by color onClick and set them to the trainList
     const [trainList, setTrainList] = useState([])
+    const [selectedLine, setSelectedLine] = useState('')
+    const [selectedStation, setSelectedStation] = useState('')
 
     const lines = ['Orange', 'Brown', 'Red', 'Blue', 'Green', 'Purple', 'Yellow']
+
+    // use the fetch methodto return all of the station from the database
+
+    // create a function to set the selected train line to the selectedLine hook
+
+    //create a function to render the dropdown for the stations if and when the line is selected
 
     return (
         <div>
