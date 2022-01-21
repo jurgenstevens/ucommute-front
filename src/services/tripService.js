f56b5d0daa6dcac069ea3762b0d1fe858d1c58b7
jurgenstevens <jurgen.stevens@yahoo.com>
2022-01-21 17:54:26 -0600

    Add trip service module and post request.

--- src/services/tripService.js
+++ src/services/tripService.js
@@ -1,3 +1,18 @@
 import * as tokenService from '../services/tokenService'
-const BASE_URL = '/trips'
 
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/trips`
+
+async function createTrip(tripData) {
+  const res = await fetch(BASE_URL, {
+    headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+     },
+     body: JSON.stringify(tripData)
+  })
+  return await res.json()
+}
+
+export { 
+    createTrip
+ }
