5ba70b22666b458206a5e67de5c1165f269baac3
jurgenstevens <jurgen.stevens@yahoo.com>
2022-01-30 13:14:57 -0600

    Fix the landing page's welcome.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,17 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      {user ? 
+      <h1>Welcome back to uCommute, {user.name}!
+          <br/>
+          Let's get your commute started.
+      </h1>
+      : 
+      <h1>
+        Welcome to uCommute! 
+        <br/> 
+        Please log in or sign up to start your commute.</h1>
+      }
     </main>
   )
 }
