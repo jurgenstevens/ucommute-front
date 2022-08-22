f39cf49ef51bd6c9de524c2773150f747e1055c6
jurgenstevens <jurgen.stevens@yahoo.com>
2022-08-22 00:42:18 -0500

    Set up and export Stations Times and Selector component functions.

---
+++ src/pages/CheckTimes/StationSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StationSelector = () => {
+    return (
+        <div>
+            This is the Station Selector.
+        </div>
+    )
+}
+
+export default StationSelector
---
+++ src/pages/CheckTimes/StationTimes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StationTimes = () => {
+    return (
+        <div>
+            These are the station times.
+        </div>
+    )
+}
+
+export default StationTimes
