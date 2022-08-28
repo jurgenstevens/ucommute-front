5ea716961ca28d48d6ef01fe05052277dbd712d8
jurgenstevens <jurgen.stevens@yahoo.com>
2022-08-28 02:40:23 -0500

    Include getStop function service to fetch specific stops.

--- src/services/stopService.js
+++ src/services/stopService.js
@@ -3,13 +3,16 @@ import * as tokenService from '../services/tokenService'
 const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/stops`
 
 function getStops(){
-    return fetch(BASE_URL, {
-        headers: {'Authorization': `Bearer ${tokenService.getToken()}`
-    }
-    })
+    return fetch(BASE_URL)
+    .then(res => res.json())
+}
+
+function getStop(id){
+    return fetch(`${BASE_URL}/${id}`)
     .then(res => res.json())
 }
 
 export {
-    getStops
+    getStops,
+    getStop
 }
