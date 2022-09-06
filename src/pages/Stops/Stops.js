e153753324f7167f56afd0982610b13253527f67
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-05 23:35:46 -0500

    Remove bullets from li's.

--- src/pages/Stops/Stops.js
+++ src/pages/Stops/Stops.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import { getStop } from '../../services/stopService';
 
