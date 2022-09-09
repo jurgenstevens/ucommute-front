79086f9841fd01bf87228be1d6f72b2e325984ab
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-09 00:24:49 -0500

    Include red line image above ucommute text.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,12 +7,14 @@ const Landing = ({ user }) => {
       <h1>Start your commute, {user.name}!</h1>
       : 
       <div>
+      <div class="landingImgBox">
+        <img src="/images/ctaredwide.jpeg" alt="cta train loop" id="landingImg" />
+      </div>
       <h1>
         uCommute
         <br/> 
         Please log in or sign up to start your commute.
       </h1>
-      <img src="/gifs/ctaelboardoftrade.gif" alt="cta train loop" />
       </div>
       }
     </main>
