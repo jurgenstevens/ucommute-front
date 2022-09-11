360170f6ef5c6690b024d8af9a29835d056dfb9e
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-11 02:32:09 -0500

    Change auth form font color and login to use Bootstrap btns.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -56,9 +56,9 @@ const LoginForm = props => {
           />
         </div>
         <div>
-          <button className={styles.button}>Log In</button>
+          <button className={styles.button && 'btn btn-primary'}>Log In</button>
           <Link to="/">
-            <button>Cancel</button>
+            <button className='btn btn-danger'>Cancel</button>
           </Link>
         </div>
       </form>
