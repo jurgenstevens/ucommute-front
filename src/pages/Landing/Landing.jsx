1fb6d64094399fb1d81155947da86a488d6a088d
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-11 12:49:40 -0500

    Include new landing page image.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = ({ user }) => {
       : 
       <div>
       <div class="landingImgBox">
-        <img src="/images/ctaredwide.jpeg" alt="cta train loop" id="landingImg" />
+        <img src="/images/ctamaptransparent.png" alt="cta train loop" id="landingImg" />
       </div>
       <h1 id="landingHeader">uCommute</h1>
       </div>
