55cac708eb137f7bf8e66668ca8e8b89ce338957
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-11 14:38:05 -0500

    Include train icon to landing logo.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,10 +7,13 @@ const Landing = ({ user }) => {
       <h1>Start your commute, {user.name}!</h1>
       : 
       <div>
-      <div class="landingImgBox">
+      <div className="landingImgBox">
         <img src="/images/ctamaptransparent.png" alt="cta train loop" id="landingImg" />
       </div>
-      <h1 id="landingHeader">uCommute</h1>
+      <div className="landinglogo">
+        <img src="/images/ucommutelogo.png" alt="cta-logo" className="ucommuteLogo"/>
+        <h1 id="landingHeader"> uCommute</h1>
+      </div>
       </div>
       }
     </main>
