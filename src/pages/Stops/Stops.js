3ca879e4f54b4886b450cc1a522ce42994fe83f4
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-14 23:53:19 -0500

    Add media queries to adjust Arrival Times UI.

--- src/pages/Stops/Stops.js
+++ src/pages/Stops/Stops.js
@@ -20,54 +20,59 @@ function Stops(props) {
   let color
 	return (
 		<>
-      <img src="/images/ctaoharetermarrival.jpeg" alt="train-arrives" className='arrivalImg' />
-			<h1 className='ucommuteHeaderFont'>Type In Your Stop To Check Train Arrival Time:</h1>
-      <div className="stopSelect">
-        <Select
-          options={stops}
-          onChange={onChange}
-          defaultValue={stops[0]}
-        />
+      <div className="selectAndImage">
+        <div className="stopSelect">
+          <h1 className='ucommuteHeaderFont'>Type In Your Stop To Check Train Arrival Time:</h1>
+          <Select
+            options={stops}
+            onChange={onChange}
+            defaultValue={stops[0]}
+          />
+        </div>
+        <img src="/images/ctaoharetermarrival.jpeg" alt="train-arrives" className='arrivalImg' />
       </div>
-			{stopData ? stopData.ctatt.eta.map((stop, idx) => {
-        let timeRemaining = parseInt(stop.arrT[0].slice(10, 17).replace(":", "")) - parseInt(stopData.ctatt.tmst[0].slice(10, 17).replace(":", ""))
+      
+      <div className="timeArrivals">
+        {stopData ? stopData.ctatt.eta.map((stop, idx) => {
+          let timeRemaining = parseInt(stop.arrT[0].slice(10, 17).replace(":", "")) - parseInt(stopData.ctatt.tmst[0].slice(10, 17).replace(":", ""))
 
-        // set conditions accessing each stations rt color to change h1 style
-        if(stop.rt[0] === "Blue"){
-          color = "#21A1DE"
-        }
-        if(stop.rt[0] === "Org"){
-          color = "#F9461C"
-        }
-        if(stop.rt[0] === "G"){
-          color = "#299B3A"
-        }
-        if(stop.rt[0] === "Red"){
-          color = "#C60C30"
-        }
-        if(stop.rt[0] === "Pink"){
-          color = "#E27EA6"
-        }
-        if(stop.rt[0] === "Brn"){
-          color = "#62361B"
-        }
-        if(stop.rt[0] === "Y"){
-          color = "#F9E301"
-        }
-        if(stop.rt[0] === "P"){
-          color = "#522398"
-        }
+          // set conditions accessing each stations rt color to change h1 style
+          if(stop.rt[0] === "Blue"){
+            color = "#21A1DE"
+          }
+          if(stop.rt[0] === "Org"){
+            color = "#F9461C"
+          }
+          if(stop.rt[0] === "G"){
+            color = "#299B3A"
+          }
+          if(stop.rt[0] === "Red"){
+            color = "#C60C30"
+          }
+          if(stop.rt[0] === "Pink"){
+            color = "#E27EA6"
+          }
+          if(stop.rt[0] === "Brn"){
+            color = "#62361B"
+          }
+          if(stop.rt[0] === "Y"){
+            color = "#F9E301"
+          }
+          if(stop.rt[0] === "P"){
+            color = "#522398"
+          }
 
-        if(timeRemaining < 2){
-          return <h1 key={idx} style={{color: color}} className="stationTimes">{stop.destNm[0]}: Due</h1>
-        }
-        if(timeRemaining < 45){
-          return <h1 key={idx} style={{color: color}} className="stationTimes">{stop.destNm[0]}: {timeRemaining} min</h1>
-        } else {
-          return <h1 key={idx} style={{color: color}} className="stationTimes">{stop.destNm[0]}: Not Available</h1> 
-        }
-      })
-      : <h1 style={{color: "#1589FF"}}>No Station Chosen</h1>}
+          if(timeRemaining < 2){
+            return <h1 key={idx} style={{color: color}} className="stationTimes">{stop.destNm[0]}: Due</h1>
+          }
+          if(timeRemaining < 45){
+            return <h1 key={idx} style={{color: color}} className="stationTimes">{stop.destNm[0]}: {timeRemaining} min</h1>
+          } else {
+            return <h1 key={idx} style={{color: color}} className="stationTimes">{stop.destNm[0]}: Not Available</h1> 
+          }
+        })
+        : <h1 style={{color: "#1589FF"}}>No Station Chosen</h1>}
+      </div>
     </>
   )
 }
